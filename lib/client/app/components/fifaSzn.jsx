cae6d1a08a8b2d140bfefcb08de6718327c783c2
Tim Holmes <timothy.holmes@homes.com>
2017-09-29 15:55:23 -0400

    Worked out view switching

--- lib/client/app/components/fifaSzn.jsx
+++ lib/client/app/components/fifaSzn.jsx
@@ -15,13 +15,24 @@ class FifaSzn extends React.Component {
     this.state = {
       name: 'FIFASZN',
       active: 'LANDING',
+      currentSeason: {},
       seasons: []
     }
   }
   updateView(view) {
-    this.setState({
-      active: view
-    });
+    const newState = _.clone(this.state);
+
+    newState.active = view;
+
+    this.setState(newState);
+  }
+  selectSeason(options) {
+    const newState = _.clone(this.state);
+
+    newState.currentSeason = options.season;
+    newState.active = options.active;
+
+    this.setState(newState);
   }
   componentDidMount() {
     axios.get(`${env.API_HOST}/v1/season/list/`)
@@ -39,7 +50,13 @@ class FifaSzn extends React.Component {
         <FullScreenCard
           key={ doc.seasonId }
           header={ doc.name }
-          onClick={ () => this.updateView('SOLOSZN') }/>
+          onClick={ () => this.selectSeason(
+            {
+              season: {},
+              active: 'SOLOSZN'
+            }
+          )}
+        />
       )
     });
 
@@ -47,7 +64,12 @@ class FifaSzn extends React.Component {
       <div>
         <Header
           name={ this.state.name.toLowerCase() }
-          onClick={ () => this.updateView('LANDING') }
+          onClick={ () => this.selectSeason(
+            {
+              season: {},
+              active: 'LANDING'
+            }
+          )}
         />
           <div className="app-wrap">
             {
@@ -62,7 +84,7 @@ class FifaSzn extends React.Component {
                 </section>
               ) : this.state.active === 'NEWSZN' ? (
                 <NewSzn />
-              ) : null
+              ) : this.state.active === 'SOLOSZN' ? null : null
             }
           </div>
       </div>
